522f89a7052511a9c0395d8eace4cedffa65430a
rynidja <mr_idja@esi.dz>
2025-01-28 23:45:49 +0100

    hopefully last

--- sgph/urls.py
+++ sgph/urls.py
@@ -6,5 +6,6 @@ app_name = "sgph_api"
 urlpatterns = [
     path("", views.get_ordonnances),
     path("<int:id>/", views.get_ordonnance),
+    path("<int:consultation_id>/consult/", views.consult_ordonnance),
     path("<int:id>/valider", views.validate_ordonnance),
 ]
--- sgph/views.py
+++ sgph/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_api_key.permissions import HasAPIKey
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
+from users.permissions import can_get_obj
 
 from sgph.models import Ordonnance
 from sgph.serializers import OrdonnanceSerializer
@@ -42,3 +45,23 @@ def validate_ordonnance(request, id: int):
 
     res_serializer = OrdonnanceSerializer(instance=ordonnance)
     return Response(res_serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def consult_ordonnance(request, consultation_id):
+    """
+    Allow patient and Techincal team to view ordonnance
+    """
+    ordonnance = get_object_or_404(Ordonnance, consultation_id=consultation_id)
+
+    if not can_get_obj(request.user, ordonnance):
+        return Response(
+            {"details": "You are not allowed to see data of this user"},
+            status=status.HTTP_403_FORBIDDEN,
+        )
+
+    serializer = OrdonnanceSerializer(instance=ordonnance)
+
+    return Response({"ordonnance": serializer.data})
